7d17b49ee5f049e589b29f439f73c268d76542ad
wamalias <wardamalias@gmail.com>
2023-04-03 18:36:50 +0700

    User Configuration

--- config/database.js
+++ config/database.js
@@ -1,15 +1,15 @@
 const mysql = require('mysql');
 // buat konfigurasi koneksi
 const dbconnection = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    //password: 'password_kalian',
+    host: 'localhost',
+    user: 'was',
+    password: '123456789',
     database: 'ncc-crud',
     multipleStatements: true
 });
 // koneksi database
 dbconnection.connect((err) => {
     if (err) throw err;
-    console.log('MySQL Connected to root@127.0.0.1 in ncc-crud Database');
+    console.log('MySQL Connected to was@localhost in ncc-crud Database');
 });
 module.exports = dbconnection;
